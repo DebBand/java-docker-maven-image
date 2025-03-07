188aa90f3385007d5a1b06fc93ead5ccb5505849
DebBand <pavhawk08@gmail.com>
2025-03-06 21:27:14 -0500

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,7 +1,10 @@
 package com.mycompany.app;
 
+/**
+ * Simple Java application
+ */
 public class App {
     public static void main(String[] args) {
-        System.out.println("hanumantharao medikonda");
+        System.out.println("Hello, world!");
     }
 }
